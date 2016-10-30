84e55b406a4fbfbf23c5115479ae020760be7fbb
Chad Cunningham <Chad Cunningham>
2016-10-30 18:43:24 -0400

    Add more layouts and recycler work

--- app/src/main/java/edu/kvcc/cis298/cis298assignment3/WineItemListFragment.java
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment3/WineItemListFragment.java
@@ -1,6 +1,16 @@
 package edu.kvcc.cis298.cis298assignment3;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by ccunn on 29-Oct-16.
@@ -9,4 +19,48 @@ import android.support.v4.app.Fragment;
 public class WineItemListFragment extends Fragment {
 
     private RecyclerView mWineRecyclerView;
+    private WineItemAdapter mWineItemAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // >Inflate the list view into a new view object.
+        View view = inflater.inflate(R.layout.fragment_wine_list, container, false);
+
+        mWineRecyclerView = (RecyclerView) view.findViewById(R.id.wine_recycler_view);
+
+        // >Set the recycler view to use a new linear layout manager.
+        mWineRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        // >Update the list before rendering it.
+        updateUI();
+
+        return view;
+    }
+
+
+    private class WineItemHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView mId;
+        private TextView mDescription;
+        private TextView mPackSize;
+        private TextView mCasePrice;
+        private CheckBox mActiveCheckbox;
+
+        private WineItem mWineItem;// >The wine item to display.
+
+
+        public WineItemHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+
+            mId = (TextView) itemView.findViewById(R.id.list_item_wine_id);
+        }
+    }
+
+
+
+
+
+
+
 }
